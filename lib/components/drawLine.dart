0f6c590f9bd9f1a9a83e9811b4c955269a2363ab
mohammad <daneshjoo1396isfahan@gmail.com>
2019-07-13 15:05:15 +0430

    create draw line class for draw any line along project

---
+++ lib/components/drawLine.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class drawLine  {
+
+ get(height,widht,color){
+
+
+    return Center(
+      child: Container(
+        height:height,
+        width: widht,
+        color: color,
+
+      ),
+    );
+
+}
+
+  }
