9d0b67b4a3d1359baf1c0f834b217784a9a24844
mohammad <daneshjoo1396isfahan@gmail.com>
2019-07-13 15:19:34 +0430

    create verify page

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:art_man/page/SplashScreenPage.dart';
 import 'package:art_man/page/StdPropertyPage.dart';
 import 'package:art_man/page/SingInPage.dart';
 import 'package:art_man/page/SignUpPage.dart';
-
-/*
-import 'package:art_man/page/VerifyPage.dart';*/
+import 'package:art_man/page/VerifyPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
@@ -32,7 +30,7 @@ class MyApp extends StatelessWidget {
         '/StdPropertyBody': (context) => StdPropertyBody(),
         '/signinpage': (context) => MyCustomForm(),
         '/signuppage': (context) => SignUp(),
-        /*'/verifypage': (context) => VerifyPage(),*/
+        '/verifypage': (context) => VerifyPage(),
 
       },
     );
---
+++ lib/page/VerifyPage.dart
@@ -0,0 +1,73 @@
+import 'package:art_man/components/Bottom.dart';
+import 'package:art_man/components/InputPass.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+class VerifyPage extends StatefulWidget {
+  @override
+  myVerifyPage createState() {
+    return myVerifyPage();
+  }
+}
+
+class myVerifyPage extends State<VerifyPage> {
+  final _formkey = GlobalKey<FormState>();
+  InputPass password = new InputPass("");
+  InputPass repeat = new InputPass("");
+
+  Bottom bottom = new Bottom();
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/background.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Form(
+            key: _formkey,
+            child: Container(
+              margin: EdgeInsets.only(left: 50, right: 50),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  text("رمز عبور :"),
+                  password,
+                  text("تکرار رمز عبور :"),
+                  repeat,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+               InkWell(
+                onTap: () {
+                  Navigator.pushNamed(context, '/second');
+                  // _text.text.isEmpty ? _validate = true : _validate = false;
+                },
+                child: bottom.get("تایید و مرحله بعد", 40.0, 20.0,marginleft: 5.0,width: 120.0,
+                    startcolor: Color(0xFF5AE400),endcolor: Color(0xFF0F8F00)),
+              ),
+                  ],
+              ),
+                ],
+              ),
+            ),
+          ),
+        ),
+
+    );
+  }
+
+  Widget text(text) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 18,
+      ),
+    );
+  }
+}
