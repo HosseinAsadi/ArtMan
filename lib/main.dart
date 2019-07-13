25585fad74e2db4c71e152d96632580d527499fb
mohammad <daneshjoo1396isfahan@gmail.com>
2019-07-13 15:17:09 +0430

    create sign up class

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:art_man/page/SplashScreenPage.dart';
 import 'package:art_man/page/StdPropertyPage.dart';
 import 'package:art_man/page/SingInPage.dart';
-/*import 'package:art_man/page/SignUpPage.dart';
+import 'package:art_man/page/SignUpPage.dart';
 
-
+/*
 import 'package:art_man/page/VerifyPage.dart';*/
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -31,8 +31,8 @@ class MyApp extends StatelessWidget {
         '/':(context) => SplashScreenPage(),
         '/StdPropertyBody': (context) => StdPropertyBody(),
         '/signinpage': (context) => MyCustomForm(),
-        /*'/signuppage': (context) => SignUp(),
-        '/verifypage': (context) => VerifyPage(),*/
+        '/signuppage': (context) => SignUp(),
+        /*'/verifypage': (context) => VerifyPage(),*/
 
       },
     );
---
+++ lib/page/SignUpPage.dart
@@ -0,0 +1,104 @@
+import 'package:art_man/components/Bottom.dart';
+import 'package:art_man/components/DropDown.dart';
+import 'package:art_man/components/InputText.dart';
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  @override
+  MySingup createState() {
+    return MySingup();
+  }
+}
+
+class MySingup extends State<SignUp> {
+  final _formkey = GlobalKey<FormState>();
+  InputText name = new InputText("نام و نام خانوادگی خود را وارد نمایید...");
+  InputText phone = new InputText("شماره همراه خود را وارد نمایید...");
+  DropDown dropCountry = new DropDown("کشور محل زندگی خود را انتخاب نمایید ...");
+  DropDown dropCity = new DropDown("شهر محل زندگی خود را انتخاب نمایید ...");
+  DropDown dropsex = new DropDown("جنسیت خود را انتخاب نمایید ...");
+  Bottom bottom = new Bottom();
+
+  String countryvalue = null;
+  String cityvalue = null;
+  String sexValue = null;
+
+  @override
+  Widget build(BuildContext context) {
+    setter() {
+      setState(() {});
+    }
+
+    return  Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/background.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: ListView(
+              shrinkWrap: true,
+              children: <Widget>[
+                Center(
+                  child: Form(
+                      key: _formkey,
+                      child: Container(
+                        margin: EdgeInsets.only(left: 50, right: 50),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            text("نام و نام خانوادگی :"),
+                            name,
+                            text("انتخاب کشور :"),
+                            dropCountry,
+                            text("انتخاب شهر :"),
+                            dropCity,
+                            text("جنسیت :"),
+                            dropsex,
+                            /*dropDown.get(drop(["مرد", "زن"],
+                                "جنسیت خود را انتخاب نمایید...",)),*/
+                            text("شماره همراه :"),
+                            phone,
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                InkWell(
+                                  onTap: () {
+                                    setter();
+                                    Navigator.pushNamed(context, "/verifypage");
+                                    // _text.text.isEmpty ? _validate = true : _validate = false;
+                                  },
+                                  child: bottom.get("تایید ثبت نام", 40.0, 20.0,
+                                      marginleft: 5.0,
+                                      width: 140.0,
+                                      startcolor: Color(0xFF5AE400),
+                                      endcolor: Color(0xFF0F8F00)),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      )),
+                ),
+              ],
+            ),
+          ),
+        ),
+
+    );
+  }
+
+  Widget text(text) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 18,
+      ),
+    );
+  }
+
+
+}
